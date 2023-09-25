f33a1408ad5956620c6ed36b2a970bff70ab12c1
PabloC01 <pablocomg@gmail.com>
2023-09-25 18:03:04 +0200

    TSJ-4 first commit

---
+++ t-school-project/src/main/java/com/tsystems/pablo_canton/persistence/dto/DebugResponse.java
@@ -0,0 +1,14 @@
+package com.tsystems.pablo_canton.persistence.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class DebugResponse {
+
+    private String debugResponse;
+
+}
---
+++ t-school-project/src/main/java/com/tsystems/pablo_canton/rest/DebugRestAPIV1.java
@@ -0,0 +1,35 @@
+package com.tsystems.pablo_canton.rest;
+
+import com.tsystems.pablo_canton.business.api.debug.IDebugBusinessService;
+import com.tsystems.pablo_canton.persistence.dto.DebugResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/debug")
+public class DebugRestAPIV1 {
+
+    private static final String HELLO_WORLD = "3) This is HELLO WORLD from DebugRestAPIV1\n";
+    private final IDebugBusinessService debugBusinessService;
+
+    @Autowired
+    public DebugRestAPIV1(IDebugBusinessService debugBusinessService) {
+        this.debugBusinessService = debugBusinessService;
+    }
+
+    @GetMapping("/hello-world-string")
+    public String helloWorldString() {
+        final var helloWorld = debugBusinessService.getHelloWorld();
+        return HELLO_WORLD + (helloWorld == null ? "" : helloWorld);
+    }
+
+    @GetMapping("/hello-world-json")
+    public DebugResponse helloWorldJson() {
+        final var helloWorldStringFromBusinessService = debugBusinessService.getHelloWorld();
+        final var resultString = HELLO_WORLD + (helloWorldStringFromBusinessService == null ? "" : helloWorldStringFromBusinessService);
+        return DebugResponse.builder().debugResponse(resultString).build();
+    }
+
+}
