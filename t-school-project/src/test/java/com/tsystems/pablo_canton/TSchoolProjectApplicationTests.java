79b4cf2b57def1d678a62c586a56c5e6ce63a0e2
PabloC01 <pablocomg@gmail.com>
2023-09-25 18:18:52 +0200

    TSJ-4

---
+++ t-school-project/src/test/java/com/tsystems/pablo_canton/TSchoolProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsystems.pablo_canton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TSchoolProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
